74b22c3b0e5b47fd21e12944ef8e863421b7798b
Ken Rayner <kenneth.rayner@digital.hmrc.gov.uk>
2018-02-07 09:20:19 +0000

    put files into packages -(models and repositories)

--- src/main/scala/PetRepo/PetApp.scala
+++ src/main/scala/PetRepo/PetApp.scala
@@ -1,7 +1,15 @@
 package PetRepo
 
+import repositories.PetRepository
+
+
 object PetRepo extends App {
 
   PetRepository.all()
+
+  val fred = new models.Dog("Fred")
+  println(fred)
+  val bob = new models.Cat("Bob")
+  println(bob)
 
 }
